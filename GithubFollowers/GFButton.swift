5257faa3073ec0188a3c5fafb88bdb7ac73187b7
neddyKG <gonzalezneddy@gmail.com>
2024-01-10 08:53:07 -0400

    started tabBar, generic button and textField implementation

---
+++ GithubFollowers/GFButton.swift
@@ -0,0 +1,35 @@
+//
+//  GFButton.swift
+//  GithubFollowers
+//
+//  Created by Neddy Ksenia Gonzalez Siles on 9/1/24.
+//
+
+import UIKit
+
+class GFButton: UIButton {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    // This is initialization via storyboard
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(background: UIColor, title: String) {
+        super.init(frame:  .zero)
+        self.backgroundColor = background
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
+        
+    }
+}
---
+++ GithubFollowers/SearchVC.swift
@@ -0,0 +1,17 @@
+//
+//  SearchVC.swift
+//  GithubFollowers
+//
+//  Created by Neddy Ksenia Gonzalez Siles on 9/1/24.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+
+}
