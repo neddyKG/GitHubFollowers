7e7da6cfef3a63dfadb1c0fff5248c519d5d3770
neddyKG <gonzalezneddy@gmail.com>
2024-01-31 11:13:42 -0400

    Added header view with User info to modal (on x user tapped)

---
+++ GithubFollowers/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  GithubFollowers
+//
+//  Created by Neddy Ksenia Gonzalez Siles on 31/1/24.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
