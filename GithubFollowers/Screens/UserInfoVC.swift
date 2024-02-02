a1bd40aaab0b3d45091436e4a18c85579a0d6376
neddyKG <gonzalezneddy@gmail.com>
2024-02-02 08:02:57 -0400

    Refactored opening safari to extensions group for reusing and added weak var to UserInfoVCDelegate to avoid memory leak

--- GithubFollowers/Screens/UserInfoVC.swift
+++ GithubFollowers/Screens/UserInfoVC.swift
@@ -117,9 +117,7 @@ extension UserInfoVC: UserInfoVCDelegate {
             return
         }
         
-        let safariVC = SFSafariViewController(url: url)
-        safariVC.preferredControlTintColor = .systemGreen
-        present(safariVC, animated: true)
+        presentSafariVC(with: url)
     }
     
     func didTapGetFollowers(for user: User) {
